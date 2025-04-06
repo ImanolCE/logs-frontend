4673103d4e8429b406b65be2cd00f5053dd666ef
ImanolCE <imanolca08@gmail.com>
2025-04-06 04:19:25 -0600

    cambios en api3

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 export const API_ENDPOINTS = {
   server1: process.env.REACT_APP_API_URL || 'https://seguridad-server1.onrender.com',
-  server2: process.env.REACT_APP_API_URL || 'https://seguridad-server1.onrender.com',
+  server2: process.env.REACT_APP_API_URL || 'https://seguridad-server2.onrender.com',
   // Agrega esto para desarrollo local
   local: 'http://localhost:3001'
 };
