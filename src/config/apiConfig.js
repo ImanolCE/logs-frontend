063cb9f8dde610532fff105a38c484c525fe2aff
ImanolCE <imanolca08@gmail.com>
2025-04-13 00:51:17 -0600

    Correciones en apis

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,20 +1,20 @@
 
 // PRA RENDER 
- export const API_ENDPOINTS = {
+/* export const API_ENDPOINTS = {
   server1: process.env.REACT_APP_API_URL || 'https://seguridad-server1.onrender.com',
   server2: process.env.REACT_APP_API_URL || 'https://seguridad-server2.onrender.com',
   // Agrega esto para desarrollo local
   local: 'http://localhost:3001'
-}; 
+}; */
 
 // Configuración de API Endpoints
 // Si existe variable de entorno (.env.local) la usa (modo local)
 // Si no, usa las URL ya desplegadas (Render)
 
-/*  
+ 
 export const API_ENDPOINTS = {
   server1: process.env.REACT_APP_API_URL_SERVER1 || 'https://seguridad-server1.onrender.com',
   server2: process.env.REACT_APP_API_URL_SERVER2 || 'https://seguridad-server2.onrender.com'
 };
- */
+
 
